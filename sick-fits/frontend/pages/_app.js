247e10075830e8d059ed7d18b0dbc4d6aaf4757e
Ania Kowalska <a.kowalska622@gmail.com>
2021-12-11 16:40:55 +0100

    section 4

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,4 +1,13 @@
+import NProgress from 'nprogress';
+import Router from 'next/router';
 import { Page } from '../components/Page';
+
+// TODO: swap with our own
+import '../components/styles/nprogress.css';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 const myApp = ({ Component, pageProps }) => (
   <Page>
